d170f2ce845f0a73ce52ca05d6103cb64473fd60
unknown <crownking2006@gmail.com>
2025-06-11 14:13:30 +0200

    fix flask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -23,5 +24,5 @@ def receive_order():
     print("Нове замовлення:", data)
     return jsonify({"success": True})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
